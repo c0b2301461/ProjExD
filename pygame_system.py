8be79e27738e31b1de7621f75777ec1e883af265
Ichiishi <c0b2301461@edu.teu.ac.jp>
2024-06-11 15:36:23 +0900

    円変更

--- pygame_system.py
+++ pygame_system.py
@@ -12,7 +12,7 @@ def main():
     font = pg.font.Font(None, 80)
 
     enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    pg.draw.circle(enn, (255, 0, 0), (100, 10), 10)
     enn.set_colorkey((0, 0, 0))
 
     tmr = 0
@@ -26,7 +26,7 @@ def main():
         screen.blit(enn, [100, 400])
         pg.display.update()
         tmr += 1        
-        clock.tick(0.1)
+        clock.tick(1)
 
 
 if __name__ == "__main__":
